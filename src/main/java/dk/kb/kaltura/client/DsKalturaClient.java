80a306d9176d3c02e6460a53f15bf90f88b2f3e6
Asger Dueholm Persson <adpe@kb.dk>
2025-04-04 10:36:56 +0200

    added imports and error description file from kaltura playkit

--- src/main/java/dk/kb/kaltura/client/DsKalturaClient.java
+++ src/main/java/dk/kb/kaltura/client/DsKalturaClient.java
@@ -26,10 +26,7 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 
