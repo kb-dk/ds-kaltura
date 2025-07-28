b62b0df92685d615bf131ef53bd841c0d07ea0fb
kesj <kesj@kb.dk>
2025-07-28 10:13:46 +0200

    Rebase to refactored code

--- src/main/java/dk/kb/kaltura/client/DsKalturaClient.java
+++ src/main/java/dk/kb/kaltura/client/DsKalturaClient.java
@@ -4,6 +4,7 @@ import com.kaltura.client.enums.*;
 import com.kaltura.client.services.*;
 import com.kaltura.client.services.MediaService.*;
 import com.kaltura.client.types.*;
+import com.kaltura.client.utils.request.BaseRequestBuilder;
 import com.kaltura.client.utils.response.base.Response;
 
 import java.io.File;
@@ -178,7 +179,6 @@ public class DsKalturaClient extends DsKalturaClientBase{
 
     /**
      * Simple free form term search in Kaltura.
-     *
      * @param term a search term, such as {@code dr} or {@code tv avisen}.
      * @return a list of Kaltura IDs for matching records, empty if no hits. Max result size is {@link #BATCH_SIZE}.
      * @throws IOException if the remote request failed.
@@ -197,7 +197,6 @@ public class DsKalturaClient extends DsKalturaClientBase{
     /**
      * Generic multi search for a list of {@link ESearchEntryBaseItem items},
      * returning at most {@link #BATCH_SIZE} results.
-     *
      * @param items at least 1 search item.
      * @return the response from a Kaltura search for the given items.
      * @throws IOException if the remote request failed.
@@ -446,6 +445,244 @@ public class DsKalturaClient extends DsKalturaClientBase{
         addContentToEntry(uploadTokenId, entryId, flavorParamId);
 
         return entryId;
+    }
+
+    public Map<String,List<List<String>>> getReportTableFromSegments(List<String> segments,
+                                                           ReportInputFilter reportInputFilter) throws APIException, IOException {
+
+//        List<List<String>> rows = new ArrayList<>();
+
+        if (reportInputFilter.getEntryCreatedAtGreaterThanOrEqual() != null){
+            log.warn("Filter option EntryCreatedAtGreaterThanOrEqual will be overwritten with segments");
+        }
+        if (reportInputFilter.getEntryCreatedAtLessThanOrEqual() != null){
+            log.warn("Filter option EntryCreatedAtLessThanOrEqual will be overwritten with segments");
+        }
+
+
+        Map<String, List<List<String>>> segmentMap = new HashMap<>();
+        for(int i = 0; i < segments.size()-1; i++){
+            long start = Long.parseLong(segments.get(i));
+            long end ;
+            reportInputFilter.setEntryCreatedAtGreaterThanOrEqual(start);
+            if (i+1 > segments.size()-1) {
+                end = Long.MAX_VALUE;
+                reportInputFilter.setEntryCreatedAtLessThanOrEqual(end);
+            }else {
+                end = Long.parseLong(segments.get(i + 1))-1;
+                reportInputFilter.setEntryCreatedAtLessThanOrEqual(end);
+            }
+
+            List<List<String>> tmp = getReportTable(ReportType.TOP_CONTENT, reportInputFilter,
+                    ReportOrderBy.CREATED_AT_ASC.getValue());
+
+//            if (!tmp.isEmpty()) {
+//                if (rows.isEmpty()) { //add all content if first non-empty response
+//                    rows.addAll(tmp);
+//                }else{ //add all content but header
+//                    rows.addAll(tmp.subList(1, tmp.size()));
+//                }
+//            }
+            segmentMap.put(start+"-"+end, tmp);
+            log.debug("Done with segment {} of {}: {} - {}", i+1, segments.size(),reportInputFilter.getEntryCreatedAtGreaterThanOrEqual(),
+                    reportInputFilter.getEntryCreatedAtLessThanOrEqual());
+        }
+
+        return segmentMap;
+    }
+    public String getUrlForReportAsCsv(String title, String reportText, String headers,
+                                       ReportType reportType,
+                                       ReportInputFilter reportInputFilter, String dimension, FilterPager pager,
+                                             String order, String objectIds, ReportResponseOptions responseOptions)
+            throws IOException, APIException {
+
+        Client client = getClientInstance();
+
+        ReportService.GetUrlForReportAsCsvReportBuilder requestBuilder = ReportService.getUrlForReportAsCsv(title, reportText, headers, reportType,
+                reportInputFilter, dimension, pager, order, objectIds,  responseOptions);
+
+        Response<?> response = APIOkRequestsExecutor.getExecutor().execute(requestBuilder.build(client));
+
+        if(!response.isSuccess()){
+            throw response.error;
+        }
+        if(!(response.results instanceof String)){
+            throw new RuntimeException("Not a String");
+        }
+
+        return (String) response.results;
+    }
+
+
+    public void exportTopContent(ReportInputFilter filter, String email) throws APIException, IOException {
+
+        ReportExportItem reportExportItem = createExportItem(ReportExportItemType.TABLE, ReportType.TOP_CONTENT,
+               filter);
+        List<ReportExportItem> exportItems = new ArrayList<>();
+        exportItems.add(reportExportItem);
+
+        exportReportCSV(null, email, exportItems);
+        log.debug("Done with export TOP_CONTENT request!");
+    }
+
+    private ReportExportItem createExportItem(ReportExportItemType action,
+                                              ReportType type, ReportInputFilter filter){
+        ReportExportItem exportItem = new ReportExportItem();
+        exportItem.setAction(action);
+        exportItem.setFilter(filter);
+        exportItem.setReportType(type);
+        exportItem.setReportTitle(exportItem.getReportType().name());
+        return exportItem;
+    }
+
+    public ReportExportResponse exportReportCSV(String baseUrl, String email, List<ReportExportItem> reportExportItems) throws APIException, IOException {
+        Client client = getClientInstance();
+
+        ReportExportParams exportParams = new ReportExportParams();
+        exportParams.setRecipientEmail(email);
+        exportParams.setBaseUrl(baseUrl);
+        exportParams.setReportItems(reportExportItems);
+        exportParams.setTimeZoneOffset(-120);
+
+        var requestBuilder = ReportService.exportToCsv(exportParams);
+
+        Response<?> response = APIOkRequestsExecutor.getExecutor().execute(requestBuilder.build(client));
+
+        if(!response.isSuccess()){
+            throw response.error;
+        }
+        if(!(response.results instanceof ReportExportResponse)){
+            throw new RuntimeException("Not a String");
+        }
+
+        return (ReportExportResponse) response.results;
+
+    }
+
+    public int countAllBaseEntries(BaseEntryFilter filter) throws IOException {
+        Client client = getClientInstance();
+        BaseEntryService.CountBaseEntryBuilder count = BaseEntryService.count(filter);
+        Response<Integer> response = (Response<Integer>) APIOkRequestsExecutor.getExecutor()
+                .execute(count.build(client));
+        return response.results;
+    }
+
+    public int countAllMediaEntries(MediaEntryFilter filter) throws IOException {
+        Client client = getClientInstance();
+        MediaService.CountMediaBuilder count = MediaService.count(filter);
+        Response<Integer> response = (Response<Integer>) APIOkRequestsExecutor.getExecutor()
+                .execute(count.build(client));
+        return response.results;
+    }
+
+    public <T extends BaseEntryFilter> Set<? extends BaseEntry> listAllEntriesGeneric(T filter, BiFunction<T, FilterPager, BaseRequestBuilder> service) throws IOException {
+        Client client = getClientInstance();
+        FilterPager pager = new FilterPager();
+        pager.setPageSize(BATCH_SIZE);
+        pager.setPageIndex(1);
+
+        int maxPages = MAX_RESULT_SIZE / BATCH_SIZE;
+        Long lastCreatedTimeStamp = null;
+        int count = 0;
+
+        TreeSet<BaseEntry> allEntries = new TreeSet<>(Comparator.comparing(BaseEntry::getCreatedAt).thenComparing(BaseEntry::getId));
+
+        while (true) {
+            pager.setPageIndex(pager.getPageIndex());
+
+            if (pager.getPageIndex() > maxPages) {
+                filter.setCreatedAtGreaterThanOrEqual(lastCreatedTimeStamp);
+                pager.setPageIndex(1);
+            }
+
+            BaseRequestBuilder listBuilder = service.apply(filter, pager);
+            Response<ListResponse<BaseEntry>> response = null;
+            try {
+                response = (Response<ListResponse<BaseEntry>>) APIOkRequestsExecutor.getExecutor().execute(listBuilder.build(client));
+            } catch (Exception e) {
+                //TODO handle errors!
+                log.info("Unexpected error", e);
+            }
+
+            if (response == null || response.isEmpty()) {
+                break;
+            }
+
+            List<BaseEntry> result = response.results.getObjects();
+            count += result.size();
+            allEntries.addAll(result);
+            lastCreatedTimeStamp = allEntries.last().getCreatedAt();
+
+            log.info("Page: " + pager.getPageIndex());
+            log.info("Response.size(): {}, total received: {}", result.size(), count);
+            log.info("LatstCreatedTimeStamp: {}", lastCreatedTimeStamp);
+
+            if (result.size() < pager.getPageSize()) {
+                break;
+            }
+            pager.setPageIndex(pager.getPageIndex() + 1);
+        }
+        return allEntries;
+    }
+
+    public List<List<String>> getReportTable(ReportType reportType,  ReportInputFilter reportInputFilter,
+                                             String order, String objectIds) throws IOException, APIException {
+        //TODO: This reportedly only works up until 10000 total count even with paging. Testing indicates that it
+        // gives duplicate rows when total count is above 10000.
+        //TODO: Fix above (10000 limit), by ordering by created at and start new request from last entry. However the
+        // it seems that the query needs to have a lower than 10000 total count.
+        Client client = getClientInstance();
+        FilterPager pager = new FilterPager();
+        pager.setPageSize(BATCH_SIZE);
+
+        StringBuilder rawData = new StringBuilder();
+        int totalCount = BATCH_SIZE;
+        int index = 0;
+
+        ReportService.GetTableReportBuilder requestBuilder;
+        Response<ReportTable> response;
+
+        while(totalCount > BATCH_SIZE*index && 10000 > BATCH_SIZE*index) {
+            if (BATCH_SIZE > totalCount - BATCH_SIZE * index){
+                pager.setPageSize(totalCount - BATCH_SIZE * index);
+            }
+            index +=1;
+            pager.setPageIndex(index);
+
+            requestBuilder = ReportService.getTable(reportType, reportInputFilter,
+                    pager, order, objectIds); //Build request
+            response =
+                    (Response<ReportTable>) APIOkRequestsExecutor.getExecutor()
+                            .execute(requestBuilder.build(client));//Execute request and wait for response
+
+            if(!response.isSuccess()){ //Check if request failed
+                throw response.error;
+            } else if (response.results.getData() == null) {
+                return new ArrayList<>();
+            }
+
+            if(index == 1) { //Append header for first page
+                rawData.append(response.results.getHeader()+";");
+//                log.debug("Total Count: {}",response.results.getTotalCount());
+            }
+            totalCount = response.results.getTotalCount(); //Set total count
+            rawData.append(response.results.getData()); //Append data from page
+            log.debug("Page {} of getTable done with {} of {}", index, (index-1)*BATCH_SIZE+pager.getPageSize() ,
+                    totalCount);
+        }
+
+        List<List<String>> rows = new ArrayList<>();
+        Arrays.stream(rawData.toString().split(";")).forEach(x -> {
+            List<String> col = Arrays.stream(x.split(",")).collect(Collectors.toList());
+            rows.add(col);
+        });
+
+        return rows;
+    }
+
+    public List<List<String>> getReportTable(ReportType reportType,  ReportInputFilter reportInputFilter,
+                                             String order) throws APIException, IOException {
+        return getReportTable(reportType, reportInputFilter, order, null);
     }
 
 }
